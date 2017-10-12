3919808b7c65867c5ca0eb66fc5890da88b5bbe1
geroevvn <neron101197@mail.ru>
2017-10-13 01:00:29 +0300

    ...

--- MeshReaderUnv.cpp
+++ MeshReaderUnv.cpp
@@ -144,6 +144,7 @@ bool MeshReaderUnv::face_is_exist(set<int> face){
         if(face == set<int>((*it).begin(), (*it).end()))
             return true;
     }
+
     return false;
 }
 
@@ -153,21 +154,25 @@ bool MeshReaderUnv::edge_is_exist(int i, int j){
         if( (i == (*it)[0] && j == (*it)[1]) || (i == (*it)[1] && j == (*it)[0]) )
             return true;
     }
+
     return false;
 }
 
 void MeshReaderUnv::createFirstFace(int* indexesOfPoints, vector<int> indFace) {
 
-        int i, j, k;
+        int i, j;
         int ind1 = -1;
         int ind2;
 
         i = indFace[0];
-        for(k = 1; k < 4; k++) {
+        for(int k = 1; k < 4; k++) {
             if( !edge_is_exist(i, indFace[k]) ) {
                 j = k;
+
             }
+
             else {
+
                 if(ind1 == -1)
                     ind1 = k;
                 else
@@ -179,6 +184,7 @@ void MeshReaderUnv::createFirstFace(int* indexesOfPoints, vector<int> indFace) {
         indexesOfPoints[1] = indFace[ind1];
         indexesOfPoints[2] = indFace[j];
         indexesOfPoints[3] = indFace[ind2];
+
 }
 
 void MeshReaderUnv::read(Mesh* mesh)
@@ -221,18 +227,21 @@ void MeshReaderUnv::read(Mesh* mesh)
         mesh->createFace((*it)[0], (*it)[1], (*it)[2], (*it)[3]);
     }
 
-    int pIndexes[8];// координаты точек для создания cell(нужен определенный порядок)
+    //int pIndexes[8];// координаты точек для создания cell(нужен определенный порядок)
     int k;
-    bool temp_vector[8];// нужен для различия двух противоположных граней
+    //bool temp_vector[8];// нужен для различия двух противоположных граней
     vector<int> indFace1;
     vector<int> indFace2;
 
     for(vector<vector<int> >::iterator it = cells.begin(); it != cells.end(); ++it) {
-
+    /*
         indFace1.clear();
         indFace2.clear();
 
-        for(k = 0 ; k < 8; k++)
+        for(k = 0 ; k < 4; k++)
+          temp_vector[k] = true;
+
+        for(k = 4; k < 8; k++)
           temp_vector[k] = false;
 
         for(k = 0; k < 4; k++)
@@ -244,6 +253,7 @@ void MeshReaderUnv::read(Mesh* mesh)
 
         int i = 3;
         int j;
+       //printf("%d | ",++io);
 
         while( !face_is_exist(set<int>(indFace1.begin(), indFace1.end())) && i != -1) { // Генерация сочетания без повторений для нахождения грани
 
@@ -253,10 +263,10 @@ void MeshReaderUnv::read(Mesh* mesh)
                for(j = i+1; j < 4; j++)
                   vectIndexes[j] = vectIndexes[i] + j - i;
 
-               i = k - 1;
+               i = 3;
             }
             else
-                i--;
+               { i--; continue;}
 
             indFace1.clear();
             for(k = 0; k < 8; k++)
@@ -268,17 +278,120 @@ void MeshReaderUnv::read(Mesh* mesh)
             }
         }
 
-        if(i == -1)
-        {
-         ///Error (не получается грань)
-        }
+            //if(!face_is_exist(set<int>(indFace1.begin(),indFace1.end()))) { printf("Sad1 | ");}
 
         for(k = 0; k < 8; k++)
             if( !temp_vector[k] )
                 indFace2.push_back((*it)[k]);
 
-        createFirstFace(pIndexes, indFace1);
 
+         if(!face_is_exist(set<int>(indFace2.begin(),indFace2.end()))) {printf("Sad2 | ");}
+
+        createFirstFace(pIndexes, indFace1);
+        */
+
+        indFace1.clear();
+        indFace2.clear();
+
+         for(k = 0; k < 4; k++) {
+            indFace1.push_back((*it)[k]);
+            indFace2.push_back((*it)[k + 4]);
+        }
+
+
+        if( !face_is_exist(set<int>(indFace1.begin(), indFace1.end())) ) {
+
+            faces.push_back(vector<int>(indFace1.begin(), indFace1.end()));
+            mesh->createFace(indFace1[0], indFace1[1], indFace1[2], indFace1[3]);
+        }
+
+
+         if( !face_is_exist(set<int>(indFace2.begin(), indFace2.end())) ) {
+
+            faces.push_back(vector<int>(indFace2.begin(), indFace2.end()));
+            mesh->createFace(indFace2[0], indFace2[1], indFace2[2], indFace2[3]);
+        }
+
+        set<int> Face;
+
+        Face.insert(indFace1[0]); Face.insert(indFace1[1]);
+        Face.insert(indFace2[0]); Face.insert(indFace2[1]);
+
+        if( !face_is_exist(Face) ) {
+
+            faces.push_back(vector<int>(Face.begin(), Face.end()));
+            mesh->createFace(indFace1[0], indFace1[1], indFace2[0], indFace2[1]);
+        }
+
+        Face.clear();
+
+        Face.insert(indFace1[2]); Face.insert(indFace1[3]);
+        Face.insert(indFace2[2]); Face.insert(indFace2[3]);
+
+        if( !face_is_exist(Face) ) {
+
+            faces.push_back(vector<int>(Face.begin(), Face.end()));
+            mesh->createFace(indFace1[2], indFace1[3], indFace2[2], indFace2[3]);
+        }
+
+        Face.clear();
+
+        Face.insert(indFace1[1]); Face.insert(indFace1[2]);
+        Face.insert(indFace2[1]); Face.insert(indFace2[2]);
+
+        if( !face_is_exist(Face) ) {
+
+            faces.push_back(vector<int>(Face.begin(), Face.end()));
+            mesh->createFace(indFace1[1], indFace1[2], indFace2[1], indFace2[2]);
+        }
+
+        Face.clear();
+
+        Face.insert(indFace1[0]); Face.insert(indFace1[3]);
+        Face.insert(indFace2[0]); Face.insert(indFace2[3]);
+
+        if( !face_is_exist(Face) ) {
+
+            faces.push_back(vector<int>(Face.begin(), Face.end()));
+            mesh->createFace(indFace1[0], indFace1[3], indFace2[0], indFace2[3]);
+        }
+
+
+        vector<int> temp;
+
+        for(k = 0; k < 4; k++) {
+            temp.clear();
+
+            if( !edge_is_exist(indFace1[k],indFace1[(k+1)%4]) ) {
+
+                temp.push_back(indFace1[k]);
+                temp.push_back(indFace1[ (k+1)%4 ]);
+                edges.push_back(temp);
+                mesh->createEdge(temp[0],temp[1]);
+            }
+
+            temp.clear();
+            if( !edge_is_exist(indFace2[k],indFace2[(k+1)%4]) ) {
+
+                temp.push_back(indFace2[k]);
+                temp.push_back(indFace2[ (k+1)%4 ]);
+                edges.push_back(temp);
+                mesh->createEdge(temp[0],temp[1]);
+            }
+
+            temp.clear();
+            if( !edge_is_exist(indFace1[k],indFace2[k]) ) {
+
+                temp.push_back(indFace1[k]);
+                temp.push_back(indFace2[k]);
+                edges.push_back(temp);
+                mesh->createEdge(temp[0],temp[1]);
+            }
+        }
+
+
+        mesh->createCell(indFace1[0], indFace1[1], indFace1[2], indFace1[3], indFace2[0], indFace2[1], indFace2[2], indFace2[3]);
+        /*
         for(k = 0; k < 4; k++) {
             for(vector<int>::iterator fit2 = indFace2.begin(); fit2 != indFace2.end(); ++fit2) {
                 if( edge_is_exist(pIndexes[k], *fit2) ) {
@@ -287,9 +400,11 @@ void MeshReaderUnv::read(Mesh* mesh)
                 }
             }
         }
-
-        mesh->createCell(pIndexes[0], pIndexes[1], pIndexes[2], pIndexes[3], pIndexes[4], pIndexes[5], pIndexes[6], pIndexes[7]);
+        */
+        //mesh->createCell(pIndexes[0], pIndexes[1], pIndexes[2], pIndexes[3], pIndexes[4], pIndexes[5], pIndexes[6], pIndexes[7]);
+        //printf("%d %d %d %d %d %d %d %d\n",pIndexes[0], pIndexes[1], pIndexes[2], pIndexes[3], pIndexes[4], pIndexes[5], pIndexes[6], pIndexes[7]);
     }
+
 }
 
 
